fd2015ef30ef1fe5ddceb18767fdde3b9bdcfa50
seddie95 <sed@live.ie>
2020-07-31 10:59:10 +0100

    update urls with localization features

--- django_heroku_test/urls.py
+++ django_heroku_test/urls.py
@@ -14,10 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
 from pages.views import HomeView
+from django.views.i18n import JavaScriptCatalog
 
 urlpatterns = [
+    path('i18n/', include('django.conf.urls.i18n')),
     path('admin/', admin.site.urls),
     path('', HomeView.as_view(), name='home'),
+]
+
+urlpatterns += [
+    re_path(r'^jsi18n/$',
+            JavaScriptCatalog.as_view(packages=['pages.jscripti18n']),
+            name='javascript-catalog'),
 ]
