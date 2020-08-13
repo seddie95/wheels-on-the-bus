19ab1496829644ae099440c9ef75487a99015c56
seddie95 <sed@live.ie>
2020-08-13 13:09:54 +0100

    Add logging

--- django_heroku_test/settings.py
+++ django_heroku_test/settings.py
@@ -166,3 +166,23 @@ CACHES = {
         }
     }
 }
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'debug.log'),
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
